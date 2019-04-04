866500827f8a9f463f20ade259b14fc27336dc93
Kevin Galligan <galligankevinp@gmail.com>
2019-04-04 21:03:52 +0100

    Starting point: use Flask-Pages and Flask-Freeze.

---
+++ README.md
@@ -0,0 +1,17 @@
+## Instructions
+All scripts executed from the base directory.
+
+Execute `./setup.sh` once.
+
+Execute `./run.sh` to start Flask webserver.
+
+Execute `./run.sh build` to build static copy of site in `build` folder.
+
+Execute `./run.sh deploy` to build static copy of site on `master` branch.
+
+Requires Python3.
+
+## Todo List
+* Style, content of site.
+* Custom 404 page(s).
+* Domain name, point to site.
--- app.py
+++ app.py
@@ -1,50 +1,26 @@
-from flask import Flask
-from flask import render_template
-import jinja2_highlight
+import sys
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
-# Meta data about blog posts.
-# Post ID, date (YYYY-MM-DD), title, subtitle.
-# Newest comes first.
-BLOG_POSTS = [
-    ("partitioning", "2018-11-13", "How to Partition Data in a Database")
-]
+FLATPAGES_EXTENSION = '.md'
 
-POST_ID_TO_DETAILS = {post_id: (date, title)
-                      for post_id, date, title in BLOG_POSTS}
-
-class TamingTheMachine(Flask):
-    jinja_options = dict(Flask.jinja_options)
-    jinja_options.setdefault('extensions', []).append('jinja2_highlight.HighlightExtension')
-
-app = TamingTheMachine(__name__)
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route('/')
-@app.route('/index.html')
-@app.route('/home')
-def main_page():
-    return render_template('home.html', posts=BLOG_POSTS)
+def index():
+    return render_template('index.html', pages=pages)
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html', posts=BLOG_POSTS)
+@app.route('/<path:path>')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
 
-@app.route('/blog/<post_id>')
-def blog_post(post_id):
-    if post_id not in POST_ID_TO_DETAILS:
-        return render_template('blog-post-does-not-exist.html')
-    date, title  = POST_ID_TO_DETAILS[post_id]
-    return render_template(
-        'blog-posts/{}.html'.format(post_id),
-        date=date,
-        title=title)
-
-@app.route('/apps')
-def projects():
-    return render_template('apps.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
